0038697153ee6876d3df0b2476d170837a5f1c6e
daskousik2223 <das.kousik2223@gmail.com>
2022-04-11 00:04:15 -0400

    Commit - 04112022 - 1204am

---
+++ src/test/java/com/springtut/db/springadb/SpringadbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtut.db.springadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringadbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
